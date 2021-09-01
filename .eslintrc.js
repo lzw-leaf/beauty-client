f75dcd4ae709afa95e7ac48973af7aa1e8a21b55
刘志伟 <lzw@spacetech.com.cn>
2021-09-01 16:54:13 +0800

    ui组件改用vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
     // 允许显式any
     '@typescript-eslint/no-explicit-any': ['off'],
     // 允许非显式返回值类型
-     "@typescript-eslint/explicit-function-return-type": 0
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
     // 关闭非空断言
     '@typescript-eslint/no-non-null-assertion': 0
   }
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import store from './store'
 import {setTitle} from './utils'
 // 全局样式文件
 import '@/style/index.scss'
+import vuetify from './plugins/vuetify'
 Vue.config.productionTip = false
 Vue.prototype.$setTitle = setTitle
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
