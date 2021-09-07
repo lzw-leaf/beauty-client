5101bb486d524a5834b91e3776dc20f58b04901e
刘志伟 <lzw@spacetech.com.cn>
2021-09-07 16:25:48 +0800

    代理更改

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,7 @@ module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
     proxy: {
-      // '/api/': {
-      //   target: 'http://192.168.0.101:8080/',
-      //   pathRewrite: {'^/api': ''}
-      // }
-      '/api/': {
-        target: 'http://beauty.anshuye.cn/',
-        pathRewrite: {'^/api': '/beauty'}
-      }
+      '/api/': 'http://beauty.anshuye.cn'
     }
   }
 }
